22fbfb6bf02a2eb32148a50e7f93fbc042e98bfd
Megan Lee <meganlee.id@gmail.com>
2017-07-20 22:00:20 -0700

    more graph clone

--- src/main/java/com/meganlee/CopyListRandomPointer.java
+++ src/main/java/com/meganlee/CopyListRandomPointer.java
@@ -5,78 +5,61 @@ import java.util.Map;
 
 public class CopyListRandomPointer {
     //------------------- Solution 1 --------------------------//
-    // O(n) time, O(1) space
-    // use the old structure to guide the new structure
-    public RandomListNode copyRandomList(RandomListNode head) {
-        // input validation
-        if (head == null) {
-            return head;
-        }
-
-        // step 1: interleave new clone of node after the old one,
+    // O(n) time, O(n) space: Hashmap<old -> new>
+    public RandomListNode copyRandomList2(RandomListNode head) {
+        // if head == null, the following code works well
+        // 1st time loop: create map{old -> new}
+        Map<RandomListNode, RandomListNode> map = new HashMap<>();
         RandomListNode iter = head;
         while (iter != null) {
-            RandomListNode afterIter = iter.next;
-            RandomListNode newNode = new RandomListNode(iter.label);
-            iter.next = newNode;
-            newNode.next = afterIter;
-            newNode.random = iter.random;
-            iter = afterIter;
+            map.put(iter, new RandomListNode(iter.label));
+            iter = iter.next;
+        }
+        // 2nd time loop: fill in next and random for new nodes
+        iter = head;
+        while (iter != null) {
+            RandomListNode iterClone = map.get(iter);
+            iterClone.next = map.get(iter.next);     // map.get(key)  returns value (if key exist)
+            iterClone.random = map.get(iter.random); // map.get(null) returns null  (if key no there)
+            iter = iter.next;
+        }
+        // return the new head
+        return map.get(head);
+    }
+
+    //------------------- Solution 2   --------------------------//
+    // O(n) time, O(1) space: old new INTERLEAVES
+    public RandomListNode copyRandomList(RandomListNode head) {
+        // if head == null, the following code works well
+        // step 1: interleave old and new
+        RandomListNode iter = head;
+        while (iter != null) {
+            RandomListNode iterClone = new RandomListNode(iter.label);
+            iterClone.next = iter.next;
+            iter.next = iterClone;
+            iter = iter.next.next; // jump 2 nodes
         }
 
-        // step 2: link the new node's random link to the right one
+        // step 2: correct the new node's random link
         iter = head;
         while (iter != null) {
             if (iter.random != null) {  // REMEMBER FOR checking!!
                 iter.next.random = iter.random.next;
             }
-            iter = iter.next.next;
+            iter = iter.next.next; // jump 2 nodes
         }
 
         // step 3: separate two list
-        RandomListNode newHead = head.next;
+        RandomListNode newHead = (head == null) ? null : head.next; // record it for return, consider null
         iter = head;
-        while (iter != null && iter.next != null) {  // think about the condition
-            RandomListNode afterIter = iter.next;
+        while (iter != null && iter.next != null) {         // loop enter condition: iter != null
+            // 0th,2nd,.. enters here to correct old next   // loop end   condition: iter.next != null
+            // 1st,3rd... enters here to correct new next
+            RandomListNode afterIter = iter.next;    
             iter.next = iter.next.next;
-            iter = afterIter;
+            iter = afterIter;  // jump 1 node
         }
-
-        // step 3: return the new head
         return newHead;
-    }
-
-
-    //------------------- Solution 2 --------------------------//
-    // O(n) time, O(n) space
-    // use a hash map to indicate which node to direct to
-    public RandomListNode copyRandomList2(RandomListNode head) {
-        // input validation
-        if (head == null) {
-            return head;
-        }
-
-        // map old node to copied node
-        RandomListNode iter = head;
-        Map<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
-        while (iter != null) {
-            map.put(iter, new RandomListNode(iter.label));
-            iter = iter.next;
-        }
-
-        // link next and random
-        iter = head;
-        while (iter != null) {
-            map.get(iter).next = map.get(iter.next);    // ATTENTION: map.get(null) --> null
-            map.get(iter).random = map.get(iter.random);
-            iter = iter.next;
-        }
-        return map.get(head);
     }
 }
 
-// 1. ERROR: Whenever you are using .next, make sure it's non-null
-// 2. ERROR: new RandomListNode() --> new RandomListNode(0)
-// 3. ERROR:    newIter.random = oldToNew.get(oldIter);
-//           -> newIter.random = oldToNew.get(oldIter.random);
-// 4. DESIGN: interleave or parallel?
